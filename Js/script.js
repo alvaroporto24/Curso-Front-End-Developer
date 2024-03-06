91f4d119130228e0ef8147f176723bc644975c63
alvaroporto24 <alvaroporto24@gmail.com>
2024-03-06 20:52:02 +0100

    Segunda clase, introducimos el menu para el movil

--- Js/script.js
+++ Js/script.js
@@ -1,8 +1,16 @@
 const emailMenu = document.querySelector('.emailMenu');
 const menuDesktop = document.querySelector('.menuDesktop');
 
-emailMenu.addEventListener('click', toggleMenuDesktop)
+const botonMenuMovil = document.querySelector('.botonMenuMovil')
+const menuMovil = document.querySelector('.menu-movil')
+
+emailMenu.addEventListener('click', toggleMenuDesktop);
+botonMenuMovil.addEventListener('click', toggleMenuMovil);
 
 function toggleMenuDesktop() {
     menuDesktop.classList.toggle('inactive')
+}
+
+function toggleMenuMovil() {
+    menuMovil.classList.toggle('inactive')
 }
