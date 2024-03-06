19be158230cb1425aaa45106f516ed396c30c80f
alvaroporto24 <alvaroporto24@gmail.com>
2024-03-06 23:33:49 +0100

    Tercera clase se añade el carrito, a su boton correspondiente

--- Js/script.js
+++ Js/script.js
@@ -4,8 +4,13 @@ const menuDesktop = document.querySelector('.menuDesktop');
 const botonMenuMovil = document.querySelector('.botonMenuMovil')
 const menuMovil = document.querySelector('.menu-movil')
 
+
+const botonCarrito = document.querySelector('.carrito')
+const carrito = document.querySelector('.cesta')
+
 emailMenu.addEventListener('click', toggleMenuDesktop);
 botonMenuMovil.addEventListener('click', toggleMenuMovil);
+botonCarrito.addEventListener('click', toggleCarrito);
 
 function toggleMenuDesktop() {
     menuDesktop.classList.toggle('inactive')
@@ -13,4 +18,8 @@ function toggleMenuDesktop() {
 
 function toggleMenuMovil() {
     menuMovil.classList.toggle('inactive')
+}
+
+function toggleCarrito() {
+    carrito.classList.toggle('inactive')
 }
