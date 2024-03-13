5bdbf646ae13a6b928f815bb8fcb8ff9b8603f71
alvaroporto24 <alvaroporto24@gmail.com>
2024-03-13 12:58:48 +0100

    Se termina de contruir el JS para que introduzca una lista de productos a html

--- Js/script.js
+++ Js/script.js
@@ -8,6 +8,8 @@ const menuMovil = document.querySelector('.menu-movil')
 const botonCarrito = document.querySelector('.carrito')
 const carrito = document.querySelector('.cesta')
 
+const cardContainer = document.querySelector('.card-container')
+
 emailMenu.addEventListener('click', toggleMenuDesktop);
 botonMenuMovil.addEventListener('click', toggleMenuMovil);
 botonCarrito.addEventListener('click', toggleCarrito);
@@ -40,3 +42,88 @@ function cerrarPantalla(pantalla) {
         pantalla.classList.add('inactive')
     }
 }
+
+let productList = [];
+productList.push({
+    name: "armario",
+    price: 120,
+    img: "https://images.pexels.com/photos/3932930/pexels-photo-3932930.jpeg?auto=compress&cs=tinysrgb&w=1600",
+
+})
+productList.push({
+    name: "armario",
+    price: 120,
+    img: "https://images.pexels.com/photos/3932930/pexels-photo-3932930.jpeg?auto=compress&cs=tinysrgb&w=1600",
+
+})
+productList.push({
+    name: "armario",
+    price: 120,
+    img: "https://images.pexels.com/photos/3932930/pexels-photo-3932930.jpeg?auto=compress&cs=tinysrgb&w=1600",
+
+})
+productList.push({
+    name: "armario",
+    price: 120,
+    img: "https://images.pexels.com/photos/3932930/pexels-photo-3932930.jpeg?auto=compress&cs=tinysrgb&w=1600",
+
+})
+productList.push({
+    name: "armario",
+    price: 120,
+    img: "https://images.pexels.com/photos/3932930/pexels-photo-3932930.jpeg?auto=compress&cs=tinysrgb&w=1600",
+
+})
+productList.push({
+    name: "Lampara",
+    price: 18,
+    img: "https://images.pexels.com/photos/3932930/pexels-photo-3932930.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    
+})
+productList.push({
+    name: "Silla",
+    price: 22,
+    img: "https://images.pexels.com/photos/3932930/pexels-photo-3932930.jpeg?auto=compress&cs=tinysrgb&w=1600",
+})
+
+function renderProduct(array) {
+    // Ciclo para mostrar todos los productos del array product list
+    for (product of array) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+
+        const productImg = document.createElement('img');
+        productImg.classList.add('product-img');
+        productImg.setAttribute('src', product.img);
+
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+
+        const productInfoDiv = document.createElement('div');
+
+        const productPrice = document.createElement('p')
+        productPrice.innerText = product.price + '€';
+        const productName = document.createElement('p')
+        productName.innerText = product.name; 
+
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', 'iconos/bt_add_to_cart.svg');
+
+        productInfoFigure.appendChild(productImgCart);
+
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+
+        cardContainer.appendChild(productCard);
+    }
+    
+}
+
+renderProduct(productList);
